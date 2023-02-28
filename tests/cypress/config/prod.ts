cafeccf0004502bdb94b033a2275a96580e51e8d
Karol Kujawski <karol.kujawski@wundermanthompson.com>
2023-02-28 18:51:39 +0100

    Add allure

--- tests/cypress/config/prod.ts
+++ tests/cypress/config/prod.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from 'cypress';
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 export default defineConfig({
   e2e: {
     viewportWidth: 1200,
+    screenshotsFolder: "build/cypress/screenshots",
     baseUrl: "https://d39x7kisu5mvy0.cloudfront.net",
     setupNodeEvents(on, config) {
+      allureWriter(on, config);
       config.video = false;
       return config;
     },
     env: {
+      allure: true,
+      allureResultsPath: "build/allure-results",
       apiUrl: "https://0bortdj0g9.execute-api.eu-central-1.amazonaws.com"
     }
   }
