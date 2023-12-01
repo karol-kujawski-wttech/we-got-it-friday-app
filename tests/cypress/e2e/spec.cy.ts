707ae30f0f66d38d5a277ec88d8450c0496545cb
Karol Kujawski <karol.kujawski@wundermanthompson.com>
2023-12-01 10:03:45 +0100

    Starting point for mentoring 1.12.2023

--- tests/cypress/e2e/spec.cy.ts
+++ tests/cypress/e2e/spec.cy.ts
@@ -1,6 +1,50 @@
 describe('Saturday App', () => {
   
-  it('displays a baseUrl', () => {
-    cy.visit('/');
+  it('displays an info that today is not Saturday', () => {
+    cy.intercept('GET', '/isSaturday/*', {
+      statusCode: 200,
+      body: false
+    })
+      .visit('/')
+      .get('#isSaturday')
+      .should('have.text', 'Sobota? To nie dzisiaj :(')
   });
+
+  it('displays an info that today is Saturday', () => {
+    cy.intercept('GET', '/isSaturday/*', {
+      statusCode: 200,
+      body: true
+    })
+      .visit('/')
+      .get('#isSaturday')
+      .should('have.text', 'Dzisiaj jest sobota! :D')
+  });
+
+  it('displays how long should I wait for Saturday', () => {
+    cy.intercept('GET', '/minutesToSaturday/*', {
+      statusCode: 200,
+      body: 4321
+    })
+    cy.visit('/')
+      .get('#button')
+      .click()
+      .get('#minutes')
+      .should('have.text', 'Zostało 4321 minut do soboty.')
+  });
+});
+
+describe('Friday App API', () => {
+
+  it('returns 200 for isSaturday', () => {
+    cy.request('GET', 'https://cidayq1sbj.execute-api.eu-central-1.amazonaws.com/isSaturday/1700830144570').then((response) => {
+      expect(response.status).eq(200);
+    })
+  });
+
+  it('returns 200 for minutesToSaturday', () => {
+    cy.request('GET', 'https://cidayq1sbj.execute-api.eu-central-1.amazonaws.com/minutesToSaturday/1700830144570').then((response) => {
+      expect(response.status).eq(200);
+    })
+  });
+
 });
