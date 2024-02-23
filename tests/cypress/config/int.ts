bd9cbb8fdcdc48f3bf14c95b791f050bbcaaf18b
Karol Kujawski <karol.kujawski@wundermanthompson.com>
2024-02-23 10:30:31 +0100

    add percy

--- tests/cypress/config/int.ts
+++ tests/cypress/config/int.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'cypress';
+import cypressSplit = require('cypress-split');
 
 export default defineConfig({
-  projectId: "5b2u4f",
   e2e: {
     viewportWidth: 1200,
     baseUrl: "https://d1695jp1dqp7q6.cloudfront.net",
     video: false,
     setupNodeEvents(on, config) {
+      cypressSplit(on, config);
       return config;
     },
     env: {
