8e8acb06f26fc72a916b61187d6b0387c4d823e7
Karol Kujawski <karol.kujawski@wundermanthompson.com>
2024-02-26 18:34:41 +0100

    Starting point - vol.3

--- tests/cypress/config/prod.ts
+++ tests/cypress/config/prod.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'cypress';
-import cypressSplit = require('cypress-split');
 
 export default defineConfig({
   e2e: {
@@ -7,7 +6,6 @@ export default defineConfig({
     baseUrl: "https://d3hp4tguev37fw.cloudfront.net",
     video: false,
     setupNodeEvents(on, config) {
-      cypressSplit(on, config);
       return config;
     },
     env: {
